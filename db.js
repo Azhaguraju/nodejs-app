e00f600150a7b3453e8067b602896a6779e533ec
Azhaguraju Pandi <azhaguking@gmail.com>
2024-12-29 01:41:37 +0530

    db code updated

--- db.js
+++ db.js
@@ -1,12 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/employee', {useNewUrlParser: true}, 
-(err) => {
+// Localhost DB
+mongoose.connect(
+  "mongodb://localhost:27017/employee",
+  { useNewUrlParser: true },
+
+// Atlas DB
+// mongoose.connect(
+//   "mongodb+srv://azhagu1010:Handsome2103@devcluster.lxju4.mongodb.net/?retryWrites=true&w=majority&appName=DevCluster",
+//   { useNewUrlParser: true },
+  (err) => {
     if (!err) {
-        console.log('Successfully Established Connection with MongoDB')
+      console.log("Successfully Established Connection with MongoDB");
+    } else {
+      console.log(
+        "Failed to Establish Connection with MongoDB with Error: " + err
+      );
     }
-    else {
-        console.log('Failed to Establish Connection with MongoDB with Error: '+ err)
-    }
-});
+  }
+);
 module.exports = mongoose;
